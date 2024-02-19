cfdbb0591a3a26da7a18ee017ab21621fbd47523
cheatingthemichal <52798998+cheatingthemichal@users.noreply.github.com>
2024-02-18 21:43:39 -0500

    syntax = :

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ exports.handler = async (event) => {
             gameSession.gameStarted = true;
             gameSession.gameInProgress = true;
             message += " Minimum number of players reached. Game started!";
-            updateExpression = 'SET players = :players, playerCount = :playerCount, gameStarted = :gameStarted, pot = :pot, currentTurn = :currentTurn, gameStage = :gameStage, gameInProgress = :gameInProgress, deck = : deck, highestBet = :highestBet, bettingStarted = :bettingStarted';
+            updateExpression = 'SET players = :players, playerCount = :playerCount, gameStarted = :gameStarted, pot = :pot, currentTurn = :currentTurn, gameStage = :gameStage, gameInProgress = :gameInProgress, deck = :deck, highestBet = :highestBet, bettingStarted = :bettingStarted';
             expressionAttributeValues[':gameStarted'] = gameSession.gameStarted;
             expressionAttributeValues[':pot'] = gameSession.pot;
             expressionAttributeValues[':currentTurn'] = gameSession.currentTurn;
